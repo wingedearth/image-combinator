111664b22cc53477d58083045fe1c883d575c383
Andrew Anissi <andrew.anissi@nbcuni.com>
2018-07-15 14:35:25 -0700

    add image previews with remove button

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,3 +1,4 @@
 export const DEFAULT_PORT = 3000;
 export const MAIN_TITLE = 'Image Combinator';
 export const PUBLIC_PATH = '/build/';
+export const MAX_IMAGE_LENGTH = 4;
---
+++ src/client/components/preview.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../css/preview.scss';
+
+class Preview extends Component {
+	onComponentDidUnmount() {
+		window.URL.revokeObjectURL(this.props.image.preview);
+	}
+
+	removeImage(e, index) {
+		const { actions } = this.props;
+
+		actions.removeImage(index);
+	}
+
+	render() {
+		const { index } = this.props;
+		const id = `preview-${index}`;
+		const image = this.props.image;
+		// const { store, actions } = this.props;
+
+		return (
+			<div id={id} className="preview">
+				<p className="preview__text">Image: {index + 1}</p>
+				<button className="preview__button-remove" onClick={e => this.removeImage(e, index)}>Remove</button>
+				<img className="preview__img" src={image.preview} />
+			</div>
+		);
+	}
+}
+
+Preview.propTypes = {
+	index: PropTypes.number,
+	image: PropTypes.object,
+	actions: PropTypes.object
+};
+
+export default Preview;
