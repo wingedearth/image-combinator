5606861967f62b4f21f088cee43403849bb3043f
Andrew Anissi <andrew.anissi@nbcuni.com>
2018-07-16 00:00:38 -0700

    add image merging

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -1,9 +1,28 @@
 import express from 'express';
 import Controller from '../controllers/controller';
+import mergeImg from 'merge-img';
 
 const router = express.Router({ strict: true });
 
 // Get Main Page
+router.post('/merged', function(req, res) {
+	const { images } = req.body;
+	console.log('about to merge');
+
+	mergeImg(images).then((img) => {
+		console.log('img:', img);
+
+		// Save image as file
+		// img.write('out.png', () => console.log('done'));
+
+		// // Get image as `Buffer`
+		// img.getBuffer(img.getMIME(), (err, buf) => {
+		// 	if (err) throw err;
+
+		// 	res.send(buf);
+		// });
+	});
+});
 router.get('/', Controller);
 router.get('*', Controller);
 
